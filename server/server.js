e6bee22e5842a112c17fd690f68dc4f5a8d9bd35
Anuj0113 <anujdevansh72@gmail.com>
2026-04-09 16:45:58 +0530

    render ready

--- server/server.js
+++ server/server.js
@@ -32,3 +32,7 @@ app.use('/api/ai', aiRouter)
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
